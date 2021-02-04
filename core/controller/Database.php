272d96b1f5272a18d82d850797a16274e1ba92fb
Juanfranciscodasilva <56869952+Juanfranciscodasilva@users.noreply.github.com>
2021-02-04 08:40:00 +0100

    Update Database.php

--- core/controller/Database.php
+++ core/controller/Database.php
@@ -9,7 +9,7 @@ class Database {
 
 	function connect(){
 		//Copiar aquí también la info de la conexión
-		$this->user="b8ba9d94f28aff";$this->pass="a8337514";$this->host="eu-cdbr-west-02.cleardb.net";$this->ddbb="heroku_f51a3950727cd14";
+		$this->user="b643f2c84e3efe";$this->pass="5a5e4169";$this->host="eu-cdbr-west-03.cleardb.net";$this->ddbb="heroku_3709d33bc5d959a";
 		$con = new mysqli($this->host,$this->user,$this->pass,$this->ddbb);
 		$con->query("set sql_mode=''");
 		return $con;
